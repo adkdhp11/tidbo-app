8b7089d3e392a82d42701caef2eb54777e007f06
Jo <Keen@jos-imac.home>
2020-01-18 01:32:56 +0000

    Updates

---
+++ precache-manifest.7d997a7c39ae4210ebb2d2d966f56c76.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "135f495300e7a17395f9d674b0966693",
+    "url": "/tidbo-app/index.html"
+  },
+  {
+    "revision": "8c0f157d49d601566f2e",
+    "url": "/tidbo-app/static/css/main.a97a7588.chunk.css"
+  },
+  {
+    "revision": "d12548ac0cc8fbbd0385",
+    "url": "/tidbo-app/static/js/2.9f671d62.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/tidbo-app/static/js/2.9f671d62.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c0f157d49d601566f2e",
+    "url": "/tidbo-app/static/js/main.d0a88c2c.chunk.js"
+  },
+  {
+    "revision": "abdb5d8c92f8bc9f8a82",
+    "url": "/tidbo-app/static/js/runtime-main.19c46654.js"
+  },
+  {
+    "revision": "f1409621e35aad241b9f504eae1d5bf3",
+    "url": "/tidbo-app/static/media/tidbo_logo_color.f1409621.svg"
+  },
+  {
+    "revision": "8d8c7db71371ec379ca785b0c8f4df9a",
+    "url": "/tidbo-app/static/media/tm_back.8d8c7db7.svg"
+  },
+  {
+    "revision": "8659515094d85059439811bbedcbbc39",
+    "url": "/tidbo-app/static/media/tm_ham_menu.86595150.svg"
+  }
+]);
